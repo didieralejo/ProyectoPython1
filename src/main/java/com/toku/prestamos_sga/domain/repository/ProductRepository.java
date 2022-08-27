15dda97d058cc73156134aba91215b48cba4d97b
didieralejo <didieralejo@gmail.com>
2022-08-27 17:17:44 -0500

    ProductoRepository se orienta a DOMAIN con ProductRepository

--- src/main/java/com/toku/prestamos_sga/domain/repository/ProductRepository.java
+++ src/main/java/com/toku/prestamos_sga/domain/repository/ProductRepository.java
@@ -13,7 +13,7 @@ public interface ProductRepository {
     //Regla del dominio, cuando cualquier repositorio quiera acceder a la tabla producto de la BD.
 
     List<Product> getAll(); // Retornar todos los prodyctos
-    Optional<List<Product>> getByCategory(int id_categoria); //Retorna lista de procdcutos por id_categoria
+    Optional<List<Product>> getByCategory(int idCategoria); //Retorna lista de procdcutos por id_categoria
     Optional<List<Product>> getScarseProducts(int cantidadStock, boolean estado); //Retorna lista de productos escasos
     Optional<Product> getProduct(int idProducto); //Retorna el producto dado su id
     Product save(Product product); //Guardar producto
--- src/main/java/com/toku/prestamos_sga/persistence/ProductoRepository.java
+++ src/main/java/com/toku/prestamos_sga/persistence/ProductoRepository.java
@@ -1,56 +1,82 @@
 package com.toku.prestamos_sga.persistence;
 
+import com.toku.prestamos_sga.domain.Product;
+import com.toku.prestamos_sga.domain.repository.ProductRepository;
 import com.toku.prestamos_sga.persistence.crud.ProductoCrudRepository;
 import com.toku.prestamos_sga.persistence.entity.Producto;
+import com.toku.prestamos_sga.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository //Indicamos que esta clase esta interactuando con BD
-public class ProductoRepository {
+/**
+ * Cuando se implemente ProductRepository, el repositorio ProductoRepository queda
+ * enfocado al DOMAIN y no ha una tabla puntual PRODUCTO
+ */
+public class ProductoRepository implements ProductRepository{
 
     //Objeto de la interfaz
     private ProductoCrudRepository productoCrudRepository;
+    //Variable para el mapeo
+    private ProductMapper productMapper;
 
-    //Metodo para recuperar los productos de la base de datos
-    public List<Producto> gelAll(){
+    @Override
+    //Lista de todos los productos
+    public List<Product> getAll(){
         //productoCrudRepository.findAll() retorna Iterable, se debe casting (List<Producto>)
-        return (List<Producto>) productoCrudRepository.findAll();
+        List<Producto> productoList = (List<Producto>) productoCrudRepository.findAll();
+        return productMapper.toProducts(productoList);
     }
 
-    public List<Producto> getByCategoria (int idCategoria) {
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    //Trae una lista de productos por categoria
+    public Optional<List<Product>> getByCategory(int idCategoria) {
+        List<Producto> productoList = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+        return Optional.of(productMapper.toProducts(productoList));
+    }
+
+    @Override
+    /**
+     * Busca los productos escasos y que esten activos
+     * productoList.map(prods -> productMapper.toProducts(prods));
+     * Permite sacar del Optional productoList, la List<Producto> y se almacena en prods y luego se envia a
+     * productMapper.toProducts(prods)
+     */
+    public Optional<List<Product>> getScarseProducts(int cantidadStock, boolean estado) {
+        Optional<List<Producto>> productoList = productoCrudRepository.findByCantidadStockLessThanAnAndEstado(cantidadStock, estado);
+        return productoList.map(prods -> productMapper.toProducts(prods));
+    }
+
+    @Override
+    //Buscar producto por id
+    public Optional<Product> getProduct(int idProducto) {
+        Optional<Producto> producto =  productoCrudRepository.findById(idProducto);
+        return producto.map(prod -> productMapper.toProduct(prod));
+    }
+
+    @Override
+    //Guadar producto
+    public Product save(Product product) {
+        Producto producto = productMapper.toProducto(product);
+        return productMapper.toProduct(productoCrudRepository.save(producto));
+    }
+
+    @Override
+    //Eliminar por id
+    public void deleteByIdProducto(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+
+    @Override
+    //Eliminar teniendo el objeto completo
+    public void delete(Product product) {
+        productoCrudRepository.delete(productMapper.toProducto(product));
     }
 
     public Producto getByCodigoBarras(String codigoBarras){
         return productoCrudRepository.findBycodigoBarras(codigoBarras);
     }
-
-    public Optional<List<Producto>> getProductosLimitieEnStock (int cantidadStock, boolean estado){
-        return productoCrudRepository.findByCantidadStockLessThanAnAndEstado(cantidadStock, estado);
-    }
-
-    //Buscar producto por id
-    public Optional<Producto> getProducto(int idProducto){
-        return productoCrudRepository.findById(idProducto);
-    }
-
-    //Guadar producto
-    public Producto saveProduct (Producto producto){
-        return productoCrudRepository.save(producto);
-    }
-
-    //Eliminar teniendo el objeto completo
-    public void deleteProduct (Producto producto){
-        productoCrudRepository.delete(producto);
-    }
-
-    //Eliminar por id
-    public void deleteProductById (int idProducto){
-        productoCrudRepository.deleteById(idProducto);
-    }
-
-
 
 }
