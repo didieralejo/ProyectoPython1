ad762c888e991b3e706dda5258ad55c0245af2d9
didieralejo <didieralejo@gmail.com>
2022-08-27 13:31:13 -0500

    Archivos iniciales

---
+++ src/main/java/com/toku/prestamos_sga/PrestamosSgaApplication.java
@@ -0,0 +1,13 @@
+package com.toku.prestamos_sga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosSgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosSgaApplication.class, args);
+	}
+
+}
